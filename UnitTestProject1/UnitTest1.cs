6cc980556cfebfadcb5bb175a361a438d1a987a1
swagner <swagner@shutterfly.com>
2017-11-13 09:55:39 -0700

    added tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Barcode2DTest
+    {
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double value = 11.99;
+            double expected = 11.99;
+
+            Assert.AreEqual(expected, value, 0.001, "Account not debited correctly");
+        }
+    }
+}
